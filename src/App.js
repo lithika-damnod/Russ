bb529d52398b3dbf911e712ca46502dce8e09e7e
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2023-01-12 16:39:19 +0530

    created landing page animation using framer-motion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Hero from "./components/Hero"; 
 
 function App() {
   return (
-    <div></div>
+    <div className="main-wrapper">
+      <Hero />
+    </div> 
   );
 }
 
---
+++ src/components/Hero.js
@@ -0,0 +1,60 @@
+import { motion } from "framer-motion"; 
+import "./Hero.css"; 
+
+function Hero() { 
+    return (
+        <div className="hero-container">
+            <motion.h3 className="hero-title"
+                initial={{ opacity: 0, scale: 0.7 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.3,
+                  ease: [0, 0.71, 0.2, 1.01]
+                }} 
+            >
+                Russ
+            </motion.h3>
+            <motion.h5 className="hero-description"
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }} 
+                transition={{ 
+                    duration: 0.5, 
+                    delay: 0.3, 
+                }}
+            >
+                Get instant answers to your questions about any text with Passage Q&A - an AI-powered reading companion that analyzes and summarizes any text you provide and answer questions based on the information in the passage
+            </motion.h5>
+            <motion.div className="try-it-btn-wrapper"
+                initial={{ y: 50 }}
+                animate={{ y: 0 }}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ 
+                    type: "spring", 
+                    stiffness: 200, 
+                    damping: 17,
+                }} 
+            >
+                <span className="text">
+                    Try It
+                </span>
+                <motion.span class="material-symbols-rounded"
+                    animate={{ 
+                        y: [1, -2.5, 1] 
+                    }} 
+                    transition={{
+                        duration: 0.5, 
+                        ease: "easeInOut", 
+                        repeat: Infinity, 
+                        repeatDelay: 0.6, 
+                    }}
+                >
+                    call_made
+                </motion.span>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Hero; 
