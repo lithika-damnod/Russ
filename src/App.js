19adc5b8c2ac8dea5279963c32a37c7f6fd7eb26
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2023-01-21 08:31:12 +0530

    changing state on main App.js now changes the view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
+import {useState} from "react"; 
+// css 
 import './App.css';
+// components 
 import Hero from "./components/Hero"; 
 import StepOne from "./components/StepOne";  
 import CommonNav from "./components/CommonNav"; 
 import TextInput from './components/TextInput';
 import FileInput from './components/FileInput';
+import QAView from './components/QAView';
 
 function App() {
+  // veiw states 
+  const [heroVisibility, setHeroVisibility] = useState(true);  // initially true 
+  const [stepOneVisibility, setStepOneVisibility] = useState(false);   
+  const [textInputVisibility, setTextInputVisibility] = useState(false);   
+  const [fileInputVisibility, setFileInputVisibility] = useState(false);   
+  const [QAViewVisibility, setQAViewVisibility] = useState(false);   
+
   return (
     <div className="main-wrapper">
-      <Hero />
+
+      { heroVisibility && <Hero />}
+
+      { stepOneVisibility && (
+        <>
+          <CommonNav />
+          <StepOne />
+        </>
+      )}
+
+      { textInputVisibility && (
+        <>
+          <CommonNav />
+          <TextInput />
+        </>
+      )}
+
+      { fileInputVisibility && (
+        <>
+          <CommonNav />
+          <FileInput />
+        </>
+      )}
+
+      { QAViewVisibility && (
+        <>
+          <CommonNav />
+          <QAView />
+        </>
+      )}
+
     </div> 
   );
 }
