76a41437f2b1f4766e07cd27126f8058ab3f0780
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2023-02-01 23:53:59 +0530

    added progressing circle to show up while extracting text from the image

--- src/components/Verify.js
+++ src/components/Verify.js
@@ -1,4 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
+import { motion } from "framer-motion"; 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState, useEffect } from "react"; 
+
+// mui 
+import CircularProgress from '@mui/material/CircularProgress';
 
 // reducers 
 import { showQAView, showFileInput } from "../features/view/viewSlice"; 
@@ -13,7 +19,26 @@ import Button from "./Button";
 function Verify() { 
     const dispatch = useDispatch(); 
 
+    // redux states 
     const scannedText = useSelector((state) => state.scan.text); 
+
+    // states 
+    const [progressVisibility, setProgressVisibility] = useState(true); // initially true  
+     
+    useEffect(() => { 
+        if(scannedText !== "") { 
+            setProgressVisibility(false); 
+        }
+    }, [scannedText])
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#000000',
+                darker: '#000000',
+            },
+        }
+    }); 
 
     return (
         <>
@@ -21,7 +46,27 @@ function Verify() {
                 <div className="prompt">
                     Please review the text extracted from images for accuracy and legibility.
                 </div>
-                <textarea className="input-textarea" defaultValue={scannedText} onChange={e => dispatch(setScanResults(e.target.value))}></textarea>
+                { progressVisibility && (
+                    <div className="progress-wrapper">
+                        <motion.div
+                            initial={{ opacity: 0}} 
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 2 }}
+                        >
+                            <ThemeProvider theme={theme}>
+                                <CircularProgress disableShrink color="primary" size={60} thickness={2} />
+                            </ThemeProvider>
+                        </motion.div>
+                    </div>
+                )}
+                { !progressVisibility && (
+                    <motion.textarea className="input-textarea" defaultValue={scannedText} onChange={e => dispatch(setScanResults(e.target.value))}
+                        initial={{ opacity: 0}} 
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                    ></motion.textarea>
+                )}
                 <div className="position-controllers">
                     <Button id="prev-btn" backgroundColor="#000000cf"
                         onClick={() => dispatch(showFileInput())}
